76c3e67208d940c141f83fe17241199277b9956d
roni <ronald.dev@gmail.com>
2023-10-20 20:05:16 -0300

    Desadio Dio Aprimorando Calculadora IMC

---
+++ android/app/src/main/kotlin/com/example/dio_desafio_imc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dio_desafio_imc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'home/homepage.dart';
+
+void main() {
+  runApp(const HomePage());
+}
